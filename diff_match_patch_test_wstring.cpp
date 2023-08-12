86be1be4a21d0a1e88a6d9113e5df17a2502d705
root <108691043+razvan-pricop-ptt@users.noreply.github.com>
2023-08-12 15:41:17 +0300

    test: count function

--- diff_match_patch_test_wstring.cpp
+++ diff_match_patch_test_wstring.cpp
@@ -637,6 +637,68 @@ class diff_match_patch_test : diff_match_patch<wastring> {
     // Test null inputs -- not needed because nulls can't be passed in string_t&.
   }
 
+  void testDiffMainOnlyCount() {
+    // Perform a trivial diff.
+    assertEquals("diff_main_only_count: Null case.", 0, dmp.diff_main_only_count("", "", false));
+
+    assertEquals("diff_main: Equality.", 0, dmp.diff_main_only_count("abc", "abc", false));
+
+    assertEquals("diff_main: Simple insertion.", 3, dmp.diff_main_only_count("abc", "ab123c", false));
+
+    assertEquals("diff_main: Simple deletion.", 3, dmp.diff_main_only_count("a123bc", "abc", false));
+
+    assertEquals("diff_main: Two insertions.", 6, dmp.diff_main_only_count("abc", "a123b456c", false));
+
+    assertEquals("diff_main: Two deletions.", 6, dmp.diff_main_only_count("a123b456c", "abc", false));
+
+    // Perform a real diff.
+    // Switch off the timeout.
+    dmp.Diff_Timeout = 0;
+    assertEquals("diff_main: Simple case #1.", 2, dmp.diff_main_only_count("a", "b", false));
+
+    assertEquals("diff_main: Simple case #2.", 14, dmp.diff_main_only_count("Apples are a fruit.", "Bananas are also fruit.", false));
+
+    assertEquals("diff_main: Overlap #1.", 6, dmp.diff_main_only_count("1ayb2", "abxab", false));
+
+    assertEquals("diff_main: Overlap #2.", 6, dmp.diff_main_only_count("abcy", "xaxcxabc", false));
+
+    assertEquals("diff_main: Overlap #3.", 22, dmp.diff_main_only_count("ABCDa=bcd=efghijklmnopqrsEFGHIJKLMNOefg", "a-bcd-efghijklmnopqrs", false));
+
+    assertEquals("diff_main: Large equality.", 13, dmp.diff_main_only_count("a [[Pennsylvania]] and [[New", " and [[Pennsylvania]]", false));
+
+    dmp.Diff_Timeout = 0.1f;  // 100ms
+    // This test may 'fail' on extremely fast computers.  If so, just increase the text lengths.
+    string_t a = "`Twas brillig, and the slithy toves\nDid gyre and gimble in the wabe:\nAll mimsy were the borogoves,\nAnd the mome raths outgrabe.\n";
+    string_t b = "I am the very model of a modern major general,\nI've information vegetable, animal, and mineral,\nI know the kings of England, and I quote the fights historical,\nFrom Marathon to Waterloo, in order categorical.\n";
+    // Increase the text lengths by 1024 times to ensure a timeout.
+    for (int x = 0; x < 10; x++) {
+      a = a + a;
+      b = b + b;
+    }
+    clock_t startTime = clock();
+    dmp.diff_main_only_count(a, b);
+    clock_t endTime = clock();
+    // Test that we took at least the timeout period.
+    assertTrue("diff_main: Timeout min.", dmp.Diff_Timeout * CLOCKS_PER_SEC <= endTime - startTime);
+    // Test that we didn't take forever (be forgiving).
+    // Theoretically this test could fail very occasionally if the
+    // OS task swaps or locks up for a second at the wrong moment.
+    // Java seems to overrun by ~80% (compared with 10% for other languages).
+    // Therefore use an upper limit of 0.5s instead of 0.2s.
+    assertTrue("diff_main: Timeout max.", dmp.Diff_Timeout * CLOCKS_PER_SEC * 2 > endTime - startTime);
+    dmp.Diff_Timeout = 0;
+
+    // Test the linemode speedup.
+    // Must be long to pass the 100 char cutoff.
+    a = "1234567890\n1234567890\n1234567890\n1234567890\n1234567890\n1234567890\n1234567890\n1234567890\n1234567890\n1234567890\n1234567890\n1234567890\n1234567890\n";
+    b = "abcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\nabcdefghij\n";
+    assertEquals("diff_main: Simple line-mode.", dmp.diff_main_only_count(a, b, true), dmp.diff_main_only_count(a, b, false));
+
+    a = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+    b = "abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij";
+    assertEquals("diff_main: Single line-mode.", dmp.diff_main_only_count(a, b, true), dmp.diff_main_only_count(a, b, false));
+    // Test null inputs -- not needed because nulls can't be passed in string_t&.
+  }
 
   //  MATCH TEST FUNCTIONS
 
